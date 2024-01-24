81b92235fe62e73941f6dd5ed87a945fac638610
shailendra08 <shailendra.jain0894@gmail.com>
2024-01-24 17:38:58 -0500

    Signup Page completed with functionality and login page design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-// import Login from "./components/Auth-Components/Login";
-import SignUp from "./components/Auth-Components/SignUp";
+import Login from "./components/Auth-Components/Login";
+// import SignUp from "./components/Auth-Components/SignUp";
 import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
       <ToastContainer/>
-      <SignUp />
+      <Login />
+      {/* <SignUp /> */}
     </>
   );
 }
--- src/components/Auth-Components/Login.jsx
+++ src/components/Auth-Components/Login.jsx
@@ -1,46 +1,72 @@
-function Login() {
+import { useState } from "react";
 
-  
+
+function Login() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const usenavigate = useNavigate()
+
+  const handleLogin = (e) => {
+    console.log(e)
+    e.preventDefault();
+  }
+
+  const newUser = () => {
+ 
+  }
 
   return (
     <div>
-      <div className="offset-lg-3 col-lg-6 mt-20">
+      <div className="container offset-lg-3 col-lg-6 mt-2">
+        <div className="card">
+          <div className="card-header text-center">
+            <h2>Welcome to Conference Scheduler</h2>
+          </div>
+        </div>
+      </div>
+      <div className="offset-lg-3 col-lg-6 mt-2">
         <div className="container">
-          <div className="card ">
-            <div className="card-header">
-              <h1>User Login</h1>
-            </div>
-            <div className="card-body">
-              <div className="form-row ">
-                <div className="form-group">
-                  <label htmlFor="username">Username</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    placeholder="Ex: Abc123 Or Xyz789"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Ex: Abc45698@"
-                  />
+          <form action="" onSubmit={handleLogin}>
+            <div className="card ">
+              <div className="card-header">
+                <h1>User Login</h1>
+              </div>
+              <div className="card-body">
+                <div className="form-row ">
+                  <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input
+                      value={id}
+                      onChange={(e) => setId(e.target.value)}
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      placeholder="Ex: Abc123 Or Xyz789"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Ex: Abc45698@"
+                      autoComplete="on"
+                    />
+                  </div>
                 </div>
               </div>
+              <div className="card-footer">
+                <button type="submit" className="btn btn-primary">
+                  Login In
+                </button>
+                <button className="btn btn-success ml-3" onClick={newUser}>New User</button>
+              </div>
             </div>
-            <div className="card-footer">
-              <button type="submit" className="btn btn-primary">
-                Login In
-              </button>
-              <button className="btn btn-success ml-3">
-                New User
-              </button>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
